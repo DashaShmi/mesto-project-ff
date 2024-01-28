635e582046e4a696d44de473b7e6dbfcaf676376
Daria Dmitrienko <ishmizh@gmail.com>
2024-01-28 17:59:27 +0400

    вынесла remove

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,3 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
-
 function createCard(cardData, deleteCallback) {
   const templateElement = document.getElementById("card-template");
   const card = templateElement.content.querySelector(".card");
@@ -42,8 +5,6 @@ function createCard(cardData, deleteCallback) {
   const deleteButton = clonedCard.querySelector(".card__delete-button");
 
   deleteButton.addEventListener("click", function () {
-    clonedCard.remove();
-
     if (deleteCallback) {
       deleteCallback();
     }
@@ -62,7 +23,7 @@ const cardList = document.querySelector(".places__list");
 
 initialCards.forEach(function (cardData) {
   const createdCard = createCard(cardData, function () {
-    console.log(`delete! ${cardData.name}`);
+    createdCard.remove();
   });
   cardList.append(createdCard);
 });
