04ea58f0a2334df61f052990b730a69c611b63d5
Daria Dmitrienko <ishmizh@gmail.com>
2024-03-27 20:24:43 +0400

    модальные окна начала делать

--- src/index.js
+++ src/index.js
@@ -34,3 +34,33 @@ initialCards.forEach(function (cardData) {
   const createdCard = createCard(cardData, deleteCard);
   cardList.append(createdCard);
 });
+
+const openProfileEditButton = document.querySelector(".profile__edit-button");
+const popupCloseButton = document.querySelector(".popup__close");
+const modal = document.querySelector(".popup");
+
+// Функции для открытия/закрытия модального окна "редактирования профиля"
+
+openProfileEditButton.addEventListener("click", function () {
+  modal.style.display = "flex";
+
+  // Перебираем полученные элементы и закрываем их
+  popups.forEach(function (popup) {
+    popup.style.display = "none"; // Пример закрытия попапа путем изменения стиля
+  });
+});
+
+popupCloseButton.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+const openAddCard = document.querySelector(".profile__add-button");
+const modalMesto = document.querySelector(".popup_type_new-card");
+
+openAddCard.addEventListener("click", function () {
+  modalMesto.style.display = "flex";
+});
+
+popupCloseButton.addEventListener("click", function () {
+  modalMesto.style.display = "none";
+});
