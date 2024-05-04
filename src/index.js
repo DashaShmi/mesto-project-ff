9f45c331dd86902d7d115539125e321199b86892
Daria Dmitrienko <ishmizh@gmail.com>
2024-05-04 22:26:58 +0400

    жёёскааа, мы убрали options

--- src/index.js
+++ src/index.js
@@ -116,47 +116,48 @@ initialCards.forEach(function (cardData) {
 // убрать потом
 openPopup(profilePopup);
 
-function hideInputError(formElement, inputElement, options) {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove(options.inputErrorClass);
-  errorElement.classList.remove(options.errorClass);
-  errorElement.textContent = "-";
-}
-
-function showInputError(formElement, inputElement, errorMessage, options) {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add(options.inputErrorClass);
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add(options.errorClass);
-}
-
-const checkInputValidity = (formElement, inputElement, options) => {
-  if (inputElement.validity.valid) {
-    hideInputError(formElement, inputElement, options);
-  } else {
-    showInputError(
-      formElement,
-      inputElement,
-      inputElement.validationMessage,
-      options
-    );
-  }
-};
-
 // checkInputValidity(editProfileForm, descriptionInput);
 
-const setEventListeners = (formElement, options) => {
-  const inputList = Array.from(
-    formElement.querySelectorAll(options.inputSelector)
-  );
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener("input", function () {
-      checkInputValidity(formElement, inputElement, options);
-    });
-  });
-};
-
 function enableValidation(options) {
+  //
+  function hideInputError(formElement, inputElement) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(options.inputErrorClass);
+    errorElement.classList.remove(options.errorClass);
+    errorElement.textContent = "-";
+  }
+
+  function showInputError(formElement, inputElement, errorMessage) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(options.inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(options.errorClass);
+  }
+
+  const checkInputValidity = (formElement, inputElement) => {
+    if (inputElement.validity.valid) {
+      hideInputError(formElement, inputElement, options);
+    } else {
+      showInputError(
+        formElement,
+        inputElement,
+        inputElement.validationMessage,
+        options
+      );
+    }
+  };
+
+  const setEventListeners = (formElement) => {
+    const inputList = Array.from(
+      formElement.querySelectorAll(options.inputSelector)
+    );
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", function () {
+        checkInputValidity(formElement, inputElement, options);
+      });
+    });
+  };
+
   const formList = Array.from(document.querySelectorAll(options.formSelector));
   formList.forEach(function (formElement) {
     formElement.addEventListener("submit", function (evt) {
