0fb5ff684944981522902c0a438434e232d7b9d1
Daria Dmitrienko <ishmizh@gmail.com>
2024-05-05 16:35:15 +0400

    кнопка

--- src/components/validation.js
+++ src/components/validation.js
@@ -34,6 +34,8 @@ export function enableValidation(options) {
     inputList.forEach((inputElement) => {
       inputElement.addEventListener("input", function () {
         checkInputValidity(formElement, inputElement, options);
+        const button = formElement.querySelector(".popup__button");
+        toggleButtonState(inputList, button);
       });
     });
   };
@@ -46,3 +48,29 @@ export function enableValidation(options) {
     setEventListeners(formElement, options);
   });
 }
+
+// Функция валидности всех полей (принимает массив полей)
+const hasInvalidInput = (inputList) => {
+  // проходим по этому массиву методом some
+  return inputList.some((inputElement) => {
+    // Если поле не валидно, колбэк вернёт true
+    // Обход массива прекратится и вся функция
+    // hasInvalidInput вернёт true
+    return !inputElement.validity.valid;
+  });
+};
+
+// Функция принимает массив полей ввода
+// и элемент кнопки, состояние которой нужно менять
+const toggleButtonState = (inputList, buttonElement) => {
+  // Если есть хотя бы один невалидный инпут
+  if (hasInvalidInput(inputList)) {
+    // сделай кнопку неактивной
+    buttonElement.disabled = true;
+    buttonElement.classList.add("popup__button_inactive");
+  } else {
+    // иначе сделай кнопку активной
+    buttonElement.disabled = false;
+    buttonElement.classList.remove("popup__button_inactive");
+  }
+};
